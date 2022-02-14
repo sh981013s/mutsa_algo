469aa00b491ebf5d58f2f6063bbc380301f25340
SeungHwan-Lee <sh981013s@gmail.com>
2022-02-14 18:18:16 +0900

    feat: suspense & lazy 사용하여 loading 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
-import {
-  Home,
-  Solve,
-  Problems,
-  NewQuestion,
-  Login,
-  Submitted,
-  Console,
-  SetDisplayName,
-  SubmittedSourceCode,
-} from './pages/index';
 import GlobalStyle from './GlobalStyle';
 import { Navbar } from 'responsive-navbar-react';
 import 'responsive-navbar-react/dist/index.css';
@@ -17,6 +6,23 @@ import styled from 'styled-components';
 import { useAuthContext } from './hooks/useAuthContext';
 import isProved from './utils/provedEmails';
 import { menuProps } from './utils/constants/constants';
+import { WaveLoading } from 'react-loadingg';
+import { lazy, Suspense } from 'react';
+
+// components
+const Home = lazy(() => import('./pages/Home/Home'));
+const Solve = lazy(() => import('./pages/Solve/Solve'));
+const Problems = lazy(() => import('./pages/Problems/Problems'));
+const NewQuestion = lazy(() => import('./pages/NewQuestion/NewQuestion'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Submitted = lazy(() => import('./pages/Submitted/Submitted'));
+const Console = lazy(() => import('./pages/Console/Console'));
+const SetDisplayName = lazy(() =>
+  import('./pages/SetDisplayName/SetDisplayName')
+);
+const SubmittedSourceCode = lazy(() =>
+  import('./pages/SubmittedSourceCode/SubmittedSourceCode')
+);
 
 const Container = styled.div`
   width: 100%;
@@ -32,77 +38,81 @@ function App() {
       <GlobalStyle />
       <Navbar {...menuProps} />
       <Container>
-        {authIsReady && (
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/problems">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : (
+        <Suspense fallback={<WaveLoading />}>
+          {authIsReady && (
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/problems">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : (
+                  <Problems />
+                )}
+              </Route>
+              <Route exact path="/solve/:title">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : (
+                  <Solve />
+                )}
+              </Route>
+              <Route path="/new-question/:id?">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : !isProved(user?.email) ? (
+                  <Redirect to="/" />
+                ) : (
+                  <NewQuestion />
+                )}
+              </Route>
+              <Route exact path="/login">
+                {!user && <Login />}
+                {user && !user?.displayName.includes('20') && (
+                  <SetDisplayName />
+                )}
                 <Problems />
-              )}
-            </Route>
-            <Route exact path="/solve/:title">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : (
-                <Solve />
-              )}
-            </Route>
-            <Route path="/new-question/:id?">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : !isProved(user?.email) ? (
-                <Redirect to="/" />
-              ) : (
-                <NewQuestion />
-              )}
-            </Route>
-            <Route exact path="/login">
-              {!user && <Login />}
-              {user && !user?.displayName.includes('20') && <SetDisplayName />}
-              <Problems />
-            </Route>
-            <Route exact path="/submitted">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : user && !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : (
-                <Submitted />
-              )}
-            </Route>
-            <Route exact path="/submitted-sourcecode/:name/:title/:id">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : (
-                <SubmittedSourceCode />
-              )}
-            </Route>
-            <Route exact path="/admin">
-              {!user ? (
-                <Redirect to="/login" />
-              ) : !user?.displayName.includes('20') ? (
-                <Redirect to="/setdisplayname" />
-              ) : (
-                <Console />
-              )}
-            </Route>
-            <Route exact path="/setdisplayname">
-              {!user ? <Redirect to="/login" /> : <SetDisplayName />}
-            </Route>
-          </Switch>
-        )}
+              </Route>
+              <Route exact path="/submitted">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : user && !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : (
+                  <Submitted />
+                )}
+              </Route>
+              <Route exact path="/submitted-sourcecode/:name/:title/:id">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : (
+                  <SubmittedSourceCode />
+                )}
+              </Route>
+              <Route exact path="/admin">
+                {!user ? (
+                  <Redirect to="/login" />
+                ) : !user?.displayName.includes('20') ? (
+                  <Redirect to="/setdisplayname" />
+                ) : (
+                  <Console />
+                )}
+              </Route>
+              <Route exact path="/setdisplayname">
+                {!user ? <Redirect to="/login" /> : <SetDisplayName />}
+              </Route>
+            </Switch>
+          )}
+        </Suspense>
       </Container>
     </>
   );
